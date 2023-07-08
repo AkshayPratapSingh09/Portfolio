c0d02f3925d23a7e66a2e3a303e570f507cbaf86
Akshay <worklikeakshay@gmail.com>
2023-07-08 23:53:21 +0530

    master

---
+++ src/components/summer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function summer() {
+  return (<>
+    <div className='active'>summer</div>
+    <h1 className='head'>This is not working</h1>
+    </>
+  )
+}
+
+export default summer
