5c550699cf9582a98360bcbc6a5d2692559c8249
Akshay Pratap Singh <99736940+AkshayPratapSingh09@users.noreply.github.com>
2023-11-05 23:59:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,22 +8,6 @@ function App() {
     <>
 <Header/>
     <MainContainer/>
-    <HeroContainer/>
-    <MainContainer/>
-    <HeroContainer/>
-    <Hero2Container/>
-    <SideContainer/>
-    <SideBarContainer/>
-    <SideContainer/>
-    <SideBarContainer/>
-    <SideBar2Container/>
-    <SideBar2Container/>
-    <SideBarContainer/>
-    <SideContainer/>
-    <SideBarContainer/>
-    <SideBar2Container/>
-    <SideBar2Container/>
-    <SideBar2Container/>
     <SideBarContainerer/>
     <Footer/>
 
